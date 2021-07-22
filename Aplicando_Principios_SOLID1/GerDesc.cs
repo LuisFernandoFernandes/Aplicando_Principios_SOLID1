900806c81f6f889242e74333b7ac20b09a240b07
LuisFernandoFernandes <luisfernando.fernandes@hotmail.com>
2021-07-21 22:04:49 -0300

    Alteração de Ifs por switch e padronização dos calculos de desconto.

--- Aplicando_Principios_SOLID1/GerDesc.cs
+++ Aplicando_Principios_SOLID1/GerDesc.cs
@@ -1,28 +1,33 @@
 ﻿using Aplicando_Principios_SOLID1.Enums;
+using System;
 
 namespace Aplicando_Principios_SOLID1
 {
     public class GerenciadorDeDescontos
     {
-        public decimal AplicarDesconto(decimal precoProduto, StatusContaCliente statusContaCliente, int tempoDeContaEmAnos)
+        public decimal AplicarDesconto(decimal preco, StatusContaCliente statusContaCliente, int tempoDeContaEmAnos)
         {
             decimal precoAposDesconto = 0;
             decimal descontoPorFidelidade = (tempoDeContaEmAnos > 5) ? (decimal)5 / 100 : (decimal)tempoDeContaEmAnos / 100;
-            if (statusContaCliente == StatusContaCliente.NaoRegistrado)
+            switch (statusContaCliente)
             {
-                precoAposDesconto = precoProduto;
-            }
-            else if (statusContaCliente == StatusContaCliente.ClienteComum)
-            {
-                precoAposDesconto = (precoProduto - (0.1m * precoProduto)) - descontoPorFidelidade * (precoProduto - (0.1m * precoProduto));
-            }
-            else if (statusContaCliente == StatusContaCliente.ClienteEspecial)
-            {
-                precoAposDesconto = (0.7m * precoProduto) - descontoPorFidelidade * (0.7m * precoProduto);
-            }
-            else if (statusContaCliente == StatusContaCliente.ClienteVIP)
-            {
-                precoAposDesconto = (precoProduto - (0.5m * precoProduto)) - descontoPorFidelidade * (precoProduto - (0.5m * precoProduto));
+                case StatusContaCliente.NaoRegistrado:
+                    precoAposDesconto = preco;
+                    break;
+                case StatusContaCliente.ClienteComum:
+                    precoAposDesconto = (preco - (0.1m * preco));
+                    precoAposDesconto = precoAposDesconto - (descontoPorFidelidade * precoAposDesconto);
+                    break;
+                case StatusContaCliente.ClienteEspecial:
+                    precoAposDesconto = (preco - (0.3m * preco));
+                    precoAposDesconto = precoAposDesconto - (descontoPorFidelidade * precoAposDesconto);
+                    break;
+                case StatusContaCliente.ClienteVIP:
+                    precoAposDesconto = (preco - (0.5m * preco));
+                    precoAposDesconto = precoAposDesconto - (descontoPorFidelidade * precoAposDesconto);
+                    break;
+                default:
+                    throw new NotImplementedException();
             }
             return precoAposDesconto;
         }
