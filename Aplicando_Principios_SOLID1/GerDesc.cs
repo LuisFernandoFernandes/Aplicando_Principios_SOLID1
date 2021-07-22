ad9f8aab0b6470f4e867082b1362252292c2aea8
LuisFernandoFernandes <luisfernando.fernandes@hotmail.com>
2021-07-21 21:54:51 -0300

    Criação de Enum StatusContaCliente.

--- Aplicando_Principios_SOLID1/GerDesc.cs
+++ Aplicando_Principios_SOLID1/GerDesc.cs
@@ -1,24 +1,26 @@
-﻿namespace Aplicando_Principios_SOLID1
+﻿using Aplicando_Principios_SOLID1.Enums;
+
+namespace Aplicando_Principios_SOLID1
 {
     public class GerenciadorDeDescontos
     {
-        public decimal AplicarDesconto(decimal precoProduto, int statusContaCliente, int tempoDeContaEmAnos)
+        public decimal AplicarDesconto(decimal precoProduto, StatusContaCliente statusContaCliente, int tempoDeContaEmAnos)
         {
             decimal precoAposDesconto = 0;
             decimal descontoPorFidelidade = (tempoDeContaEmAnos > 5) ? (decimal)5 / 100 : (decimal)tempoDeContaEmAnos / 100;
-            if (statusContaCliente == 1)
+            if (statusContaCliente == StatusContaCliente.NaoRegistrado)
             {
                 precoAposDesconto = precoProduto;
             }
-            else if (statusContaCliente == 2)
+            else if (statusContaCliente == StatusContaCliente.ClienteComum)
             {
                 precoAposDesconto = (precoProduto - (0.1m * precoProduto)) - descontoPorFidelidade * (precoProduto - (0.1m * precoProduto));
             }
-            else if (statusContaCliente == 3)
+            else if (statusContaCliente == StatusContaCliente.ClienteEspecial)
             {
                 precoAposDesconto = (0.7m * precoProduto) - descontoPorFidelidade * (0.7m * precoProduto);
             }
-            else if (statusContaCliente == 4)
+            else if (statusContaCliente == StatusContaCliente.ClienteVIP)
             {
                 precoAposDesconto = (precoProduto - (0.5m * precoProduto)) - descontoPorFidelidade * (precoProduto - (0.5m * precoProduto));
             }
